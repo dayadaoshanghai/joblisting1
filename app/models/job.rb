96d5761809e13d0b1adff5f81401938959fa27c7
Johnson <974456843@qq.com>
2019-05-12 15:38:32 +0800

    add reminder for employee

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,8 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
 
+
+
   def publish!
     self.is_hidden = false
     self.save
@@ -13,4 +15,7 @@ class Job < ApplicationRecord
     self.is_hidden = true
     self.save
   end
+
+  scope :published, -> { where(is_hidden: false)}
+  scope :recent, -> { order('created_at DESC')}
 end
